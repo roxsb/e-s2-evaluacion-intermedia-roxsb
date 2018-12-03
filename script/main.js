f431fa70ddf4d3dd08703dc302c2a08c839f854d
roxsb <rox.san.bri@gmail.com>
2018-12-03 09:30:32 +0100

    bad try

---
+++ script/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+No me sale :(
+
+// document.querySelector();
+
+// let numberGuess = Math.floor((Math.random()*100)+1);
+// let contatorAnswer=0;
+
+
+// // Función que genera un número aleatorio hasta un máximo dado
+// function getRandomNumber(max) {
+//     return Math.ceil(Math.random() * max);
+//   }
+  
+//   // Anda, chacho, píntame en la consola un número random hasta 100;
+//   console.log('> ' + getRandomNumber(100));
+
+//     const numberTries = document.querySelector('.btn'); 
+    
+//     const clueNumber = document.querySelector('.clue');
+
+//     ClueNumber.innerHTML = answer;
+
+//     while(Number(answer)!==randomNumber && contador<100){
+//         if(answer<randomNumber){
+//             answer = prompt('Demasiado alto');
+//         }else{
+//             answer = prompt('Demasiado bajo');
+//         }
+//     }
+
+
+
+
+
+
+
+
+
